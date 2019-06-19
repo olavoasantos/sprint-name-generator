c76566a4d11ea7bfbf2986f09fe57f61dc0455ac
Olavo <olavo.santos@skipthedishes.ca>
2019-06-19 10:24:21 -0500

    added a generate button and a textarea for names

--- src/Generator.jsx
+++ src/Generator.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import nouns from './nouns';
 import adjectives from './adjectives';
 
 const random = (arr) => arr[Math.floor(Math.random()*arr.length)];
 
-export default ({ names = '' }) => {
-  const noun = random(nouns);
-  const adjective = random(adjectives);
-  const name = random(names.replace(', ', ',').split(','));
+export default () => {
+  const [state, setState] = useState({ title: '', names: 'Felipe, Geneva, Himanshi, Jamie, Olavo, Thyago, Jordan, Rob, Daniel, Teena, Keith, Jean' });
+  const generate = useCallback(() => {
+    const noun = random(nouns);
+    const adjective = random(adjectives);
+    const name = random(state.names.replace(', ', ',').split(','));
+    setState({ ...state, title: `${Boolean(name) ? `${name}'s ` : ''}${adjective} ${noun}` });
+  }, [state, setState]);
+
+  const setNames = useCallback(() => {
+    setState({ ...state, names: input.current.value });
+  }, [setState, state, input]);
+
+  const input = useRef();
+
+  useEffect(() => {
+    generate();
+  }, [])
+
   return (
-    <h1>
-      {Boolean(name) ? `${name}'s ` : ''}
-      {`${adjective} `}
-      {noun}
-    </h1>
+    <div>
+      <h1>{state.title}</h1>
+
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+        <textarea style={{ width: '90%', border: '1px #eee solid', padding: 16 }} ref={input} onChange={setNames} value={state.names}></textarea>
+        <div style={{ marginRight: '100%', marginTop: 16 }}>
+          <button onClick={generate} style={{ padding: '8px 16px' }}>generate</button>
+        </div>
+      </div>
+    </div>
   )
 }
